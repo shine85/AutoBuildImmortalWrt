8d45af2317958b4cff1ebed85515c856ba5bd183
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-25 15:29:30 +0800

    更新 build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -68,9 +68,9 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # PACKAGES="$PACKAGES luci-app-openclash" 【自行去掉了】
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # PACKAGES="$PACKAGES openssh-sftp-server" 【自行去掉了】
-PACKAGES="$CUSTOM_PACKAGES luci-i18n-kucat-config-zh-cn"
-PACKAGES="$CUSTOM_PACKAGES luci-app-kucat-config"
-PACKAGES="$CUSTOM_PACKAGES luci-app-autoupdate"
+PACKAGES="$PACKAGES luci-i18n-kucat-config-zh-cn"
+PACKAGES="$PACKAGES luci-app-kucat-config"
+PACKAGES="$PACKAGES luci-app-autoupdate"
 # 增加几个必备组件 方便用户安装iStore 【自行去掉了】
 # PACKAGES="$PACKAGES fdisk"
 # PACKAGES="$PACKAGES script-utils"
