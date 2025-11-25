afcec81e7f527db2ee055271c57b7b613c92ccfd
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-25 15:05:32 +0800

    更新 build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -11,7 +11,7 @@ if [ -n "$CUSTOM_PACKAGES" ]; then
   else
     # 下载 run 文件仓库
     echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-    #git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+    git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
     git clone --depth=1 https://github.com/mineextremely/store.git /tmp/store-run-repo
     #git clone --depth=1 https://github.com/soapmancn/luci-app-autoupdate.git /tmp/store-run-repo
     #git clone --depth=1 https://github.com/sirpdboy/luci-app-kucat-config.git /tmp/store-run-repo
