e9cad0b59a5ef643e1f779f951521eb3b92e0b67
shine85 <73932625+shine85@users.noreply.github.com>
2026-01-05 09:37:54 +0800

    更新 build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -73,7 +73,8 @@ PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 
 PACKAGES="$PACKAGES luci-theme-kucat"
 # PACKAGES="$PACKAGES luci-i18n-kucat-config-zh-cn"
-# PACKAGES="$PACKAGES luci-app-kucat-config" 【编译了设置没效果】
+PACKAGES="$PACKAGES luci-app-kucat-config" 
+#【编译了设置没效果】
 
 # 增加几个必备组件 方便用户安装iStore 【自行去掉了】
 # PACKAGES="$PACKAGES fdisk"
